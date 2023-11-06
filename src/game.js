8640607498aafd31701e075532362ba139cdfdc5
Peter Tempfli <peter.tempfli@deliveryhero.com>
2023-11-06 15:38:39 +0100

    x

--- src/game.js
+++ src/game.js
@@ -1,14 +1,35 @@
-import {GAMELOOP} from './gameLoop.js';
+import { GAMELOOP } from './gameLoop.js';
 
-class Game {
-    startTime;
+class Car {
     hPos = 100;
     speed = 10;
     roadPos = 0;
+    turnSpeed = 1;  
+    
+    upsert(currentT, previousT) {
+        let movedSinceLast = Math.floor((currentT - previousT) * this.speed);
+        console.log('##', currentT,previousT, movedSinceLast);
+        this.roadPos = this.roadPos + movedSinceLast;
+    }
+}
+
+class Game {
+    startTime;
+    player;
     lastUpsert = 0;
+
+    otherCars = [];
 
     constructor() {
         this.startTime = Date.now();
+        this.player = new Car();
+
+        let oc = new Car();
+        oc.roadPos = 150;
+        oc.speed = 9;
+        oc.hPos = 150;
+
+        this.otherCars.push(oc);
     }
 
     get t() {
@@ -16,41 +37,53 @@ class Game {
     }
 
     // get roadPos() {
-        // return Math.floor(this.t * this.speed);
+    // return Math.floor(this.t * this.speed);
     // }
 
-    upsertGame() {
-        console.log
-        let tt = this.t;
-        let movedSinceLast = Math.floor((tt - this.lastUpsert) * this.speed);
-        this.roadPos = this.roadPos + movedSinceLast;
+    turn(isRight) {
+        if (isRight) {
+            this.player.hPos = Math.floor(this.player.hPos + this.player.turnSpeed);
+        } else {
+            this.player.hPos = Math.floor(this.player.hPos - this.player.turnSpeed);
+        }
+        this.player.turnSpeed = this.player.turnSpeed * 1.03;
+    }
 
-        console.log('movedSinceLast', movedSinceLast);
-        console.log('this.roadPos', this.roadPos);
+    resetTurn() {
+        this.player.turnSpeed = 1;
+    }
+
+    upsertGame() {
+        let tt = this.t;
+        this.player.upsert(tt, this.lastUpsert);
+        this.otherCars.forEach(car => {
+            car.upsert(tt, this.lastUpsert);
+        });
         this.lastUpsert = tt;
     }
+
+    speedUp(isUp) {
+        if (isUp) {
+            this.player.speed++;
+        } else {
+            this.player.speed--;
+        }
+    }
+
 
 };
 
 const turn = (isRight) => (game) => {
-    if (isRight) {
-        game.hPos++;
-    } else {
-        game.hPos--;
-    }
+    game.turn(isRight)
 }
 
 const speedUp = (isUp) => (game) => {
-    if (isUp) {
-        game.speed++;
-    } else {
-        game.speed--;
-    }
+    game.speedUp(isUp);
 }
 
 const KEYMAP = {
-    'ArrowUp' : speedUp(true),
-    'ArrowDown' : speedUp(false),
+    'ArrowUp': speedUp(true),
+    'ArrowDown': speedUp(false),
     'ArrowLeft': turn(false),
     'ArrowRight': turn(true)
 }
@@ -60,12 +93,14 @@ const main = () => {
     const ctx = canvas.getContext('2d');
     let game = new Game();
 
-    console.log('st');
     document.addEventListener('keydown', (ev) => {
         if (!KEYMAP[ev.key]) return;
         KEYMAP[ev.key](game);
     })
+    document.addEventListener('keyup', (ev) => {
+        game.resetTurn();
+    });
 
-    setInterval(GAMELOOP.gameLoop(game, ctx), 100);
+    setInterval(GAMELOOP.gameLoop(game, ctx), 300);
 }
 main();
--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,17 +1,24 @@
 const XMAX = 320;
 const YMAX = 200;
 
+const BASE_Y = 150;
 
 const gameLoop = (game, ctx) => () => {
     game.upsertGame();
 
-    console.log('sp', game.speed);
+    // console.log('sp', game.speed);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, XMAX, YMAX);
 
     drawLanes(game, ctx);
     drawCar(game, ctx);
+    drawNumbers(game, ctx);
 
+    game.otherCars.forEach(car => {
+        ctx.fillStyle = 'blue';
+        let diffToPlayer = car.roadPos - game.player.roadPos;
+        ctx.fillRect(car.hPos, BASE_Y - diffToPlayer, 10, 10);            
+    });
 }
 
 const drawLanes = (game, ctx) => {
@@ -21,7 +28,7 @@ const drawLanes = (game, ctx) => {
         let rightEnd = 200;
         ctx.fillStyle = (i) % 2 == 0 ? 'red' : 'white';
 
-        let yStart = i*markerSize +(game.roadPos % (markerSize*2));
+        let yStart = i*markerSize +(game.player.roadPos % (markerSize*2));
         ctx.fillRect(leftEnd, yStart, 20, markerSize);
         ctx.fillRect(rightEnd, yStart, 20, 20);
 
@@ -30,7 +37,15 @@ const drawLanes = (game, ctx) => {
 
 const drawCar = (game, ctx) => {
     ctx.fillStyle = 'pink';
-    ctx.fillRect(game.hPos, 100, 10, 10);
+    ctx.fillRect(game.player.hPos, BASE_Y, 10, 10);
+}
+
+const drawNumbers = (game, ctx) => {
+    ctx.fillStyle = 'violet';
+    ctx.font = '10px monospace';
+    ctx.fillText(`SPEED : ${game.player.speed}`, 20, 20);
+    ctx.fillText(`POS : ${game.player.roadPos}`, 200, 20);
+
 }
 
 export const GAMELOOP = {
